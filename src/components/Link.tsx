a2e987e99c794cd67791743e94dd6a7476118f19
gabinaureche <hello@gabinaureche.com>
2020-07-17 13:48:57 +0200

    Fix Link focus style

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,4 +4,17 @@ export const Link = styled.a`
   cursor: pointer;
   color: ${(props) => props.theme.colors.primary.main};
   text-decoration: underline;
+  outline: none;
+  position: relative;
+
+  &:focus::before {
+    content: "";
+    position: absolute;
+    top: -4px;
+    right: -4px;
+    bottom: -4px;
+    left: -4px;
+    border: 2px solid ${(props) => props.theme.colors.primary.light};
+    pointer-events: none;
+  }
 `;
