0312a112444d71550bbc6bf2fbe764cec6335a89
gabinaureche <hello@gabinaureche.com>
2020-07-24 14:31:13 +0200

    Fix hero illustration background

--- src/components/pages/Home/Hero.tsx
+++ src/components/pages/Home/Hero.tsx
@@ -6,6 +6,15 @@ import { Typography } from "../../Typography";
 import { Section } from "../../Section";
 import illustrationHero from "./assets/illustrationHero.png";
 
+const HeroContainer = styled.div`
+  @media ${(props) => props.theme.breakpoints.up("medium")} {
+    background-image: linear-gradient(
+      90deg,
+      transparent 50%,
+      ${(props) => props.theme.colors.gray.light} 50%
+    );
+  }
+`;
 const HeroInnerContainer = styled(Container)`
   display: flex;
   flex-direction: column;
@@ -22,49 +31,61 @@ const HeroContent = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  @media ${(props) => props.theme.breakpoints.up("medium")} {
+    padding-right: ${(props) => props.theme.spacing(1)};
+  }
 `;
 const HeroHeading = styled(Typography).attrs({ as: "h1", variant: "h1" })`
   max-width: 26rem;
   margin-bottom: ${(props) => props.theme.spacing(4)};
 
   @media ${(props) => props.theme.breakpoints.up("medium")} {
-    margin-bottom: ${(props) => props.theme.spacing(13)};
+    margin-bottom: ${(props) => props.theme.spacing(12)};
   }
 `;
 const HeroAction = styled.div`
   text-align: center;
 `;
 const HeroIllustration = styled.div`
+  padding: ${(props) => props.theme.spacing(2)};
+  background-color: ${(props) => props.theme.colors.gray.light};
   margin-bottom: ${(props) => props.theme.spacing(2)};
 
   @media ${(props) => props.theme.breakpoints.up("medium")} {
     margin-bottom: 0;
+  }
+
+  > img {
+    display: block;
   }
 `;
 
 export function Hero() {
   return (
     <Section>
-      <HeroInnerContainer>
-        <HeroIllustration>
-          <img src={illustrationHero} alt="" width="640" />
-        </HeroIllustration>
-        <HeroContent>
-          <HeroHeading>
-            Gérer la traçabilité des déchets en toute sécurité
-          </HeroHeading>
-          <HeroAction>
-            <Button
-              variant="secondary"
-              size="large"
-              as="a"
-              href="https://trackdechets.beta.gouv.fr/signup"
-            >
-              Créer un compte
-            </Button>
-          </HeroAction>
-        </HeroContent>
-      </HeroInnerContainer>
+      <HeroContainer>
+        <HeroInnerContainer>
+          <HeroIllustration>
+            <img src={illustrationHero} alt="" width="640" />
+          </HeroIllustration>
+          <HeroContent>
+            <HeroHeading>
+              Gérer la traçabilité des déchets en toute sécurité
+            </HeroHeading>
+            <HeroAction>
+              <Button
+                variant="secondary"
+                size="large"
+                as="a"
+                href="https://trackdechets.beta.gouv.fr/signup"
+              >
+                Créer un compte
+              </Button>
+            </HeroAction>
+          </HeroContent>
+        </HeroInnerContainer>
+      </HeroContainer>
     </Section>
   );
 }
